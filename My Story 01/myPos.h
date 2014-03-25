f1413314f63bd5ae2b7c766e0302a9dd60c8253b
Jeremy <jjones9527@gmail.com>
2014-03-24 21:19:22 -0400

    Initial Commit

---
+++ My Story 01/myPos.h
@@ -0,0 +1,13 @@
+//
+//  myPos.h
+//  My Story 01
+//
+//  Created by Jeremy Jones on 2014-03-24.
+//  Copyright (c) 2014 Jeremy Jones. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface myPos : NSObject
+
+@end
---
+++ My Story 01/myPos.m
@@ -0,0 +1,13 @@
+//
+//  myPos.m
+//  My Story 01
+//
+//  Created by Jeremy Jones on 2014-03-24.
+//  Copyright (c) 2014 Jeremy Jones. All rights reserved.
+//
+
+#import "myPos.h"
+
+@implementation myPos
+
+@end
