02dfb1d34edbdb8156d3b69e91bc196f8c9c3bf1
Jeremy <jjones9527@gmail.com>
2014-03-25 20:04:29 -0400

    Final Commit

--- My Story 01/ViewController.m
+++ My Story 01/ViewController.m
@@ -7,17 +7,58 @@
 //
 
 #import "ViewController.h"
+#import "myPos.h"
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+@synthesize mapView;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [mapView setMapType:MKMapTypeStandard];
+    [mapView setZoomEnabled:YES];
+    [mapView setScrollEnabled:YES];
+    mapView.mapType=MKMapTypeHybrid;
+    MKCoordinateRegion region = { {0.0, 0.0}, {0.0, 0.0} };
+    region.center.latitude = 38.893432;
+    region.center.longitude = -104.800161;
+    region.span.longitudeDelta = 0.01f;
+    region.span.latitudeDelta = 0.01f;
+    [mapView setRegion:region animated:YES];
+    [mapView setDelegate:self];
+    
+    myPos *ann = [[myPos alloc]init];
+    ann.title = @"Dr. Rory Lewis";
+    ann.subtitle = @"University of Colorado at Colorado Springs";
+    ann.coordinate = region.center;
+    [mapView addAnnotation:ann];
+}
+
+- (MKAnnotationView *)mapView:(MKMapView *)mV viewForAnnotation:(id <MKAnnotation>)annotation
+{
+	MKPinAnnotationView *pinView = nil;
+	if(annotation != mapView.userLocation)
+	{
+		static NSString *defaultPinID = @"com.rorylewis";
+		pinView = (MKPinAnnotationView *)[mapView dequeueReusableAnnotationViewWithIdentifier:defaultPinID];
+		if ( pinView == nil )
+			pinView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:defaultPinID];
+		
+		pinView.pinColor = MKPinAnnotationColorRed;
+		pinView.canShowCallout = YES;
+		pinView.animatesDrop = YES;
+	}
+	else
+	{
+		[mapView.userLocation setTitle:@"I am here"];
+	}
+	
+    return pinView;
 }
 
 - (void)didReceiveMemoryWarning
--- My Story 01/myPos.h
+++ My Story 01/myPos.h
@@ -7,7 +7,17 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <MapKit/MKAnnotation.h>
 
 @interface myPos : NSObject
+{
+    CLLocationCoordinate2D coordinate;
+    NSString *title;
+    NSString *subtitle;
+}
+
+@property (nonatomic, assign) CLLocationCoordinate2D coordinate;
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSString *subtitle;
 
 @end
